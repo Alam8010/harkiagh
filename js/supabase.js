273d9bb04cf0509e2444a177a367116abef74627
bet rank <betrankpro@gmail.com>
2026-04-07 02:29:28 +0500

    fix api key

--- js/supabase.js
+++ js/supabase.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = 'https://gxaxkvptizbplpxzfgfc.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd4YXhrdnB0aXpicGxweHpmZ2ZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU0MjE3NDQsImV4cCI6MjA5MDk5Nzc0NH0.uR1IKYCELkca5Rw-zpVNpYx4xzn2vuwYV59shOkRw9kd';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd4YXhrdnB0aXpicGxweHpmZ2ZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU0MjE3NDQsImV4cCI6MjA5MDk5Nzc0NH0.uR1IKYCELkca5Rw-zpVNpYx4xzn2vuwYV59shOkRw9k';
 const CLOUDINARY_CLOUD = 'dzpxkndcz';
 const CLOUDINARY_PRESET = 'harkiagh';
 const { createClient } = supabase;
